84a71c39fa30ef684b20861c76724b883aa1b58e
James Talbott <jltalbott47@gmail.com>
2022-03-02 10:16:54 -0800

    Add Constant Fib to tests
     Changes to be committed:
    	modified:   tests/CompilerCodeSamplesAttribute.cs

--- tests/CompilerCodeSamplesAttribute.cs
+++ tests/CompilerCodeSamplesAttribute.cs
@@ -37,6 +37,51 @@ namespace Blaise2.Tests
                 end.
             ",
             "3.14159" };
+
+            yield return new object[] { "Constant Fibonacci", @"
+                program ConstantFib;
+
+                var x : integer;
+                    term : integer;
+                    sqrt5 : real;
+                    a : real;
+                    b : real;
+
+                begin
+                    sqrt5 := 5 ^ 0.5;
+                    a := (1 + sqrt5) / 2.0;
+                    b := (1 - sqrt5) / 2.0;
+                    writeln('Constant Fibonacci');
+                    for x := 1 to 20 do
+                    begin
+                        term := (a ^ x - b ^ x) / sqrt5;
+                        write('Term ');
+                        write(x);
+                        write(': ');
+                        writeln(term);
+                    end;;
+                end.
+            ",
+            @"Constant Fibonacci
+Term 1: 1
+Term 2: 1
+Term 3: 2
+Term 4: 3
+Term 5: 5
+Term 6: 8
+Term 7: 13
+Term 8: 21
+Term 9: 34
+Term 10: 55
+Term 11: 89
+Term 12: 144
+Term 13: 233
+Term 14: 377
+Term 15: 610
+Term 16: 987
+Term 17: 1597
+Term 18: 2584
+Term 19: 4181" };
         }
 
         public string GetDisplayName(MethodInfo methodInfo, object[] data)
